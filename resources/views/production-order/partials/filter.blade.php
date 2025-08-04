54eceb87b9f4d491d14ec428ce020adf7330ccf1
fazkha <mr.eko.handriyanto@gmail.com>
2025-08-04 14:19:54 +0700

    update dari topik sebelumnya

--- resources/views/production-order/partials/filter.blade.php
+++ resources/views/production-order/partials/filter.blade.php
@@ -58,7 +58,7 @@
         </div>
 
         <div>
-            @can('stopname-create')
+            @can('prodo-create')
                 <x-anchor-primary href="{{ route('production-order.create') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-5">
