ed9dca0dd8523ce7383285097e6562a6f1f332ae
fazkha <mr.eko.handriyanto@gmail.com>
2025-08-20 17:28:30 +0700

    fix login text dark

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -922,21 +922,21 @@
                         </svg>
                     </div>
                     @if (Route::has('login'))
-                        <nav class="-mx-3 flex flex-1 justify-end gap-2">
+                        <nav class="flex flex-1 justify-end gap-2">
                             @auth
                                 <a href="{{ url('/admin/dashboard') }}"
-                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white dark:focus-visible:ring-white">
+                                    class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white dark:focus-visible:ring-white">
                                     Dashboard
                                 </a>
                             @else
                                 <a href="{{ route('login') }}"
-                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white dark:focus-visible:ring-white">
+                                    class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white dark:focus-visible:ring-white">
                                     Login
                                 </a>
 
                                 @if (Route::has('register'))
                                     <a href="{{ route('register') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white dark:focus-visible:ring-white">
+                                        class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white dark:focus-visible:ring-white">
                                         Register
                                     </a>
                                 @endif
@@ -949,7 +949,7 @@
                 </main>
             </div>
 
-            <footer style="margin-bottom: 12px;" class="text-left text-sm text-black dark:text-white">
+            <footer style="margin-bottom: 12px;" class="text-left text-sm text-white dark:text-white">
                 <div style="margin-left: 20px;">&copy; Copyright 2025 {{ config('custom.company_name') }}.<br />All
                     rights reserved.</div>
                 {{-- <div>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</div> --}}
