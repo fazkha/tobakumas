5ee73e4440b168b864ba9ae3bb72b2ff93792443
fazkha <mr.eko.handriyanto@gmail.com>
2025-09-15 07:30:20 +0700

    try fixing browserhot at server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Policies\PurchaseReceiptPolicy;
 use App\Policies\StockOpnamePolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Spatie\LaravelPdf\PdfFactory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,6 +19,20 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        app()->bind(PdfFactory::class, function ($service, $app) {
+            return (new PdfFactory())->withBrowsershot(
+                function ($browserShot) {
+                    $browserShot->setOption(
+                        'args',
+                        [
+                            '--disable-web-security',
+                            '--allow-file-access-from-files',
+                        ],
+                    );
+                }
+            );
+        });
+
         Gate::before(function ($user, $ability) {
             return $user->hasRole('Super Admin') ? true : null;
         });
