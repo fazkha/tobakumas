0c3839dca4a8f6279480dda0a6f8453f68769d1a
fazkha <mr.eko.handriyanto@gmail.com>
2025-09-23 11:28:49 +0700

    penambahan kolom stock utk produksi>pesanan>bahan baku

--- resources/views/production-order/partials/bahanbakuproduksi.blade.php
+++ resources/views/production-order/partials/bahanbakuproduksi.blade.php
@@ -1,9 +1,18 @@
 <table class="w-full">
+    <tr>
+        <th colspan="3" class="text-right">&nbsp;</th>
+        <th class="text-right">Persediaan</th>
+    </tr>
     @foreach ($bahans as $bahan)
         <tr class="border-t border-primary-100 dark:border-primary-700">
             <td class="py-2"><span class="pl-4">{{ $bahan->bahan }}</span></td>
-            <td class="text-right">{{ number_format($bahan->jumlah, 2, ',', '.') }}</td>
             <td><span class="pl-2">{{ $bahan->satuan }}</span></td>
+            <td class="text-right">{{ number_format($bahan->jumlah, 2, ',', '.') }}</td>
+            @if ($bahan->stock < $bahan->jumlah)
+                <td class="text-right text-red-600">{{ number_format($bahan->stock, 2, ',', '.') }}</td>
+            @else
+                <td class="text-right">{{ number_format($bahan->stock, 2, ',', '.') }}</td>
+            @endif
         </tr>
     @endforeach
 </table>
