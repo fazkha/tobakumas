4ec049321710821374fb223f0e11c7ddb553b144
fazkha <mr.eko.handriyanto@gmail.com>
2025-09-24 15:45:31 +0700

    perbaikan produksi>pesanan - Part 2

--- resources/views/production-order/partials/bahanbakuproduksi.blade.php
+++ resources/views/production-order/partials/bahanbakuproduksi.blade.php
@@ -8,10 +8,14 @@
             <td class="py-2"><span class="pl-4">{{ $bahan->bahan }}</span></td>
             <td><span class="pl-2">{{ $bahan->satuan }}</span></td>
             <td class="text-right">{{ number_format($bahan->jumlah, 2, ',', '.') }}</td>
-            @if ($bahan->stock < $bahan->jumlah)
-                <td class="text-right text-red-600">{{ number_format($bahan->stock, 2, ',', '.') }}</td>
+            @if (!$datas->order->isready == 1)
+                @if ($bahan->stock < $bahan->jumlah)
+                    <td class="text-right text-red-600">{{ number_format($bahan->stock, 2, ',', '.') }}</td>
+                @else
+                    <td class="text-right">{{ number_format($bahan->stock, 2, ',', '.') }}</td>
+                @endif
             @else
-                <td class="text-right">{{ number_format($bahan->stock, 2, ',', '.') }}</td>
+                <td class="text-right">-</td>
             @endif
         </tr>
     @endforeach
