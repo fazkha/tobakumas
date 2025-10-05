1018e8759e96a11f09f978f6dd4ef32fbf07dd85
fazkha <mr.eko.handriyanto@gmail.com>
2025-10-05 07:43:02 +0700

    perbaikan laporan produksi

--- resources/views/production-order/pdf/lap-prod-one.blade.php
+++ resources/views/production-order/pdf/lap-prod-one.blade.php
@@ -141,7 +141,7 @@
             <tr>
                 <td style="width: 50%; text-align: left;">Sisa Persediaan</td>
                 <td style="width: 20px; text-align: center;">:</td>
-                <td style="width: 50%; text-align: right;">{{ number_format($jmlsis, '2', ',', '.') }}</td>
+                <td style="width: 50%; text-align: right;">-</td>
                 <td style="text-align: left;">{{ $satuan }}</td>
             </tr>
         </table>
