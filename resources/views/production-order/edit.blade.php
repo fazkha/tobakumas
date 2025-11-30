d787f14f3e6b4e4310bf5835cffdbcc5f95888bb
fazkha <mr.eko.handriyanto@gmail.com>
2025-11-30 10:51:24 +0700

    perbaikan produksi, input checkbox gabung produksi - part 11

--- resources/views/production-order/edit.blade.php
+++ resources/views/production-order/edit.blade.php
@@ -224,7 +224,7 @@
                                 <input type="checkbox" id="iscombineall" name="iscombineall" tabindex="0"
                                     class="dark:border-white-400/20 dark:scale-100 transition-all duration-500 ease-in-out dark:hover:scale-110 dark:checked:scale-100 w-5 h-5 rounded-lg shadow-md">
                                 <label for="iscombineall" class="pl-2">@lang('messages.choose')
-                                    @lang('messages.all')</label>
+                                    @lang('messages.all') ({{ count($sales) }})</label>
                             </div>
 
                             <div id="combineBody" class="grid grid-cols-1 lg:grid-cols-2 gap-2">
@@ -458,8 +458,7 @@
                         // var xstat = $('#prod_status').val();
 
                         if (xjoin) {
-                            console.log(xjoin);
-
+                            // console.log(xjoin);
                             $.ajax({
                                 url: '{{ url('/production/order/combine') }}' + "/" + xkey +
                                     "/" + xjoin,
