37682b529dafc2364ba3934f5e3163c8ce753ee4
fazkha <mr.eko.handriyanto@gmail.com>
2025-11-30 10:45:09 +0700

    perbaikan produksi, input checkbox gabung produksi - part 10

--- resources/views/production-order/partials/bahanbakuproduksi.blade.php
+++ resources/views/production-order/partials/bahanbakuproduksi.blade.php
@@ -12,7 +12,9 @@
     </tr>
     @foreach ($bahans as $bahan)
         <tr class="border-t border-primary-100 dark:border-primary-700">
-            <td class="py-2"><span class="pl-4">{{ $bahan->bahan }}</span></td>
+            <td class="py-2">
+                <p class="pl-4">{{ $bahan->bahan }}</p>
+            </td>
             <td><span class="pl-2">{{ $bahan->satuan }}</span></td>
             <td class="text-right">{{ number_format($bahan->jumlah, 2, ',', '.') }}</td>
             @if (!$isready == 1)
