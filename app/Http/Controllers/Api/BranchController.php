22fbf3872c23cc254420100c6c0a44d1f68be09d
fazkha <mr.eko.handriyanto@gmail.com>
2025-12-02 11:38:14 +0700

    penambahan api controller, branch part 14

--- app/Http/Controllers/Api/BranchController.php
+++ app/Http/Controllers/Api/BranchController.php
@@ -10,19 +10,13 @@ class BranchController extends Controller
 {
     public function getBranchList()
     {
-        $branches = Branch::where('isactive', 1)->whereNot('id', 1)->orderBy('nama')->selectRaw('id, nama as name')->get();
-
         // $branches = Branch::where('isactive', 1)->whereNot('id', 1)->orderBy('nama')->selectRaw('nama as name, id')->pluck('name', 'id')->all();
         // $branches = Branch::where('isactive', 1)->whereNot('id', 1)->orderBy('nama')->selectRaw('nama as name, id')->get()->toArray();
+        $branches = Branch::where('isactive', 1)->whereNot('id', 1)->orderBy('nama')->selectRaw('id, nama as name')->get()->toJson();
 
-        $transformedData = $branches->transform(function ($item) {
-            $item->json_column_name = json_decode($item->json_column_name);
-            return $item;
-        });
-
-        return response()->json([
+        return [
             'status' => 'success',
-            'data' => $transformedData->toJson()
-        ]);
+            'data' => $branches
+        ];
     }
 }
