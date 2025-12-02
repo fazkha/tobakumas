3218576566ab55dde7224d1af118dd03c1143851
fazkha <mr.eko.handriyanto@gmail.com>
2025-12-02 13:12:21 +0700

    penambahan api controller, branch part 19

--- app/Http/Controllers/Api/BranchController.php
+++ app/Http/Controllers/Api/BranchController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\BranchResource;
 use App\Models\Branch;
 use Illuminate\Http\Request;
 
@@ -13,14 +12,11 @@ class BranchController extends Controller
     {
         // $branches = Branch::where('isactive', 1)->whereNot('id', 1)->orderBy('nama')->selectRaw('nama as name, id')->pluck('name', 'id')->all();
         // $branches = Branch::where('isactive', 1)->whereNot('id', 1)->orderBy('nama')->selectRaw('nama as name, id')->get()->toArray();
-        // $branches = Branch::where('isactive', 1)->whereNot('id', 1)->orderBy('nama')->selectRaw('id, nama as name')->get()->toJson();
-        $branches = Branch::where('isactive', 1)->whereNot('id', 1)->orderBy('nama')->selectRaw('id, nama as name');
-        $b = new BranchResource($branches);
-        dd($b->get());
+        $branches = Branch::where('isactive', 1)->whereNot('id', 1)->orderBy('nama')->selectRaw('id, nama as name')->get()->toJson();
 
         return [
             'status' => 'success',
-            'data' => $b
+            'data' => $branches
         ];
     }
 }
