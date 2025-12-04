f5504fae0c7e460628b09c5f1fb2af2d2c4a63a9
fazkha <mr.eko.handriyanto@gmail.com>
2025-12-04 10:21:08 +0700

    penambahan api controller, rute gerobak - part 11

--- app/Http/Controllers/Api/MitraController.php
+++ app/Http/Controllers/Api/MitraController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\mm\RuteGerobak;
+use App\Models\RuteGerobak;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Config;
--- app/Models/RuteGerobak.php
+++ app/Models/RuteGerobak.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\mm;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
