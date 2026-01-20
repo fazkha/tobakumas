ba8f3cc91e10c4f6445417fb579c8020752c283b
fazkha <mr.eko.handriyanto@gmail.com>
2026-01-20 10:19:17 +0700

    perbaikan API mitra, lanjutan load omzet pekanan, part 3

--- app/Http/Controllers/Api/MitraController.php
+++ app/Http/Controllers/Api/MitraController.php
@@ -433,11 +433,11 @@ class MitraController extends Controller
             $padWeek = str($saturdayWeek)->padLeft(2, '0');
             $yearWeek = $saturdayYear . $padWeek;
             $cOmzet = $omzet[6]->rata2;
-            dd($cOmzet);
 
             if ($cOmzet) {
                 $bonus = DB::select("CALL sp_mitra_target_bonus(?)", [$cOmzet]);
                 $cBonus = $bonus[0]->bonus;
+                dd($cBonus);
 
                 if ($cBonus) {
                     $pekanan = MitraAverageOmzet::where('user_id', $data['id'])
