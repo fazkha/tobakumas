c0b785e2769daddf0bdf167f72429c683b8b7bd2
fazkha <mr.eko.handriyanto@gmail.com>
2026-01-20 16:35:57 +0700

    perbaikan API mitra, save position, part 10

--- app/Http/Controllers/Api/MitraController.php
+++ app/Http/Controllers/Api/MitraController.php
@@ -74,8 +74,6 @@ class MitraController extends Controller
         $data = $validator->validated();
 
         if (count($data['locations']) == 0) {
-            dd('1');
-
             $rute = RuteGerobak::create([
                 'user_id' => $data['id'],
                 'status' => $data['stat'],
@@ -86,8 +84,6 @@ class MitraController extends Controller
                 'timesaved' => time(),
             ]);
         } elseif ($data['locations'][0] == []) {
-            dd('2');
-
             $rute = RuteGerobak::create([
                 'user_id' => $data['id'],
                 'status' => $data['stat'],
@@ -98,9 +94,9 @@ class MitraController extends Controller
                 'timesaved' => time(),
             ]);
         } else {
-            dd('3');
-
             foreach ($data['locations'] as $location) {
+                dd($location);
+
                 try {
                     $rute = RuteGerobak::create([
                         'user_id' => $data['id'],
