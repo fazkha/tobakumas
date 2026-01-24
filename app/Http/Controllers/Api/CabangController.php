afe6f16074245472a06352bdd72066731a055b22
fazkha <mr.eko.handriyanto@gmail.com>
2026-01-24 16:44:22 +0700

    perbaikan API cabang, rute gerobak aktif, part 6

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -59,7 +59,6 @@ class CabangController extends Controller
         $prev = null;
         $tgblth = $data['tahun'] . '-' . str_pad($data['bulan'], 2, "0", STR_PAD_LEFT) . '-' . $data['tanggal'];
         $hariLalu = 'DAYNAME(rute_gerobaks.tanggal) = DAYNAME(\'' . $tgblth . '\')';
-        dd($hariLalu);
 
         try {
             $rute = RuteGerobak::join('users', 'rute_gerobaks.user_id', '=', 'users.id')
