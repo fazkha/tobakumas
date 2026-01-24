3434e630be1c27f518b9e650640202bbf86b1f88
fazkha <mr.eko.handriyanto@gmail.com>
2026-01-24 16:45:40 +0700

    perbaikan API cabang, rute gerobak aktif, part 7

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -82,6 +82,8 @@ class CabangController extends Controller
                 'message' => $e->getMessage(),
             ]);
         }
+        dd($rute);
+
 
         $maxPrice = DB::table('mitra_omzet_pengeluarans')
             ->where('user_id', $data['mitra'])
