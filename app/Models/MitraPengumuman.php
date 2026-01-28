324276db42495a99fc116ad0284415c413ca430f
fazkha <mr.eko.handriyanto@gmail.com>
2026-01-28 10:12:37 +0700

    perbaikan API mitra, penambahan pengumuman, part 1

---
+++ app/Models/MitraPengumuman.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MitraPengumuman extends Model
+{
+    protected $guarded = [];
+    protected $table = 'mitra_pengumumans';
+
+    protected $fillable = [
+        'user_id',
+        'tanggal',
+        'judul',
+        'keterangan',
+        'isactive',
+    ];
+}
---
+++ database/migrations/2026_01_28_093606_create_mitra_pengumumans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mitra_pengumumans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade');
+            $table->date('tanggal');
+            $table->string('judul', 200)->nullable();
+            $table->string('keterangan', 200)->nullable();
+            $table->unsignedTinyInteger('isactive')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mitra_pengumumans');
+    }
+};
