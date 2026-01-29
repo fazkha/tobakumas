e172164b983d2439dca4d42e8c4105c56d0df258
fazkha <mr.eko.handriyanto@gmail.com>
2026-01-29 14:22:29 +0700

    perbaikan sale invoice, single invoice, part 3

--- resources/views/sale-invoice/pdf/invoice.blade.php
+++ resources/views/sale-invoice/pdf/invoice.blade.php
@@ -162,7 +162,8 @@
                                                     <td>{{ $adonan->gerobak_id ? $adonan->gerobak->kode : ($adonan->pegawai_id ? $adonan->pegawai->nama_lengkap : ($adonan->nama_mitra ? $adonan->nama_mitra : '-')) }}
                                                     </td>
                                                     <td>
-                                                        {{ $adonan->barang->nama }}({{ str_replace('Adonan ', '', $adonan->keterangan) }})
+                                                        {{ $adonan->barang->nama }}
+                                                        ({{ str_replace('Adonan ', '', $adonan->keterangan) }})
                                                     </td>
                                                     <td style="text-align: right">
                                                         <div>
