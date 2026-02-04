55105ba5d5a8516e92f465ded831ae6468e2c344
fazkha <mr.eko.handriyanto@gmail.com>
2026-02-04 14:33:27 +0700

    perbaikan modul pegawai, enabling switch between 2 db, part 4

--- app/Http/Controllers/BrandivjabController.php
+++ app/Http/Controllers/BrandivjabController.php
@@ -14,6 +14,8 @@ use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 
 class BrandivjabController extends Controller implements HasMiddleware
 {
@@ -26,6 +28,22 @@ class BrandivjabController extends Controller implements HasMiddleware
             new Middleware('permission:brandivjab-show', only: ['show']),
             new Middleware('permission:brandivjab-delete', only: ['delete', 'destroy']),
         ];
+    }
+
+    public function db_switch($sw)
+    {
+        if ($sw == 2) {
+            Config::set('database.connections.mysql.database', config('custom.db02_dbname'));
+            Config::set('database.connections.mysql.username', config('custom.db02_username'));
+            Config::set('database.connections.mysql.password', config('custom.db02_password'));
+        } elseif ($sw == 1) {
+            Config::set('database.connections.mysql.database', config('custom.db01_dbname'));
+            Config::set('database.connections.mysql.username', config('custom.db01_username'));
+            Config::set('database.connections.mysql.password', config('custom.db01_password'));
+        }
+
+        DB::purge('mysql');
+        DB::reconnect('mysql');
     }
 
     public function index(Request $request)
@@ -47,6 +65,8 @@ class BrandivjabController extends Controller implements HasMiddleware
         }
 
         $search_arr = ['brandivjab_isactive', 'brandivjab_branch_id', 'brandivjab_division_id', 'brandivjab_jabatan_id'];
+
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(2);
 
         $branches = Branch::where('isactive', 1)->orderBy('nama')->pluck('nama', 'id');
         $divisions = Division::where('isactive', 1)->orderBy('nama')->pluck('nama', 'id');
@@ -73,6 +93,8 @@ class BrandivjabController extends Controller implements HasMiddleware
         // $datas = $datas->latest()->paginate(session('brandivjab_pp'));
         $datas = $datas->orderBy('branch_id')->orderBy('jabatan_id')->paginate(session('brandivjab_pp'));
 
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
+
         if ($request->page && $datas->count() == 0) {
             return redirect()->route('dashboard');
         }
@@ -89,6 +111,8 @@ class BrandivjabController extends Controller implements HasMiddleware
         $request->session()->put('brandivjab_jabatan_id', $request->jabatan);
 
         $search_arr = ['brandivjab_isactive', 'brandivjab_branch_id', 'brandivjab_division_id', 'brandivjab_jabatan_id'];
+
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(2);
 
         $branches = Branch::where('isactive', 1)->orderBy('nama')->pluck('nama', 'id');
         $divisions = Division::where('isactive', 1)->orderBy('nama')->pluck('nama', 'id');
@@ -117,6 +141,8 @@ class BrandivjabController extends Controller implements HasMiddleware
 
         $datas->withPath('/general-affair/brandivjab'); // pagination url to
 
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
+
         $view = view('brandivjab.partials.table', compact(['datas', 'branches', 'divisions', 'jabatans']))->with('i', (request()->input('page', 1) - 1) * session('brandivjab_pp'))->render();
 
         if ($view) {
@@ -128,16 +154,22 @@ class BrandivjabController extends Controller implements HasMiddleware
 
     public function create(): View
     {
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(2);
+
         $branches = Branch::where('isactive', 1)->orderBy('nama')->pluck('nama', 'id');
         $divisions = Division::where('isactive', 1)->orderBy('nama')->pluck('nama', 'id');
         $jabatans = Jabatan::where('isactive', 1)->orderBy('nama')->pluck('nama', 'id');
         $atasans = Brandivjab::where('isactive', 1)->orderBy('jabatan_id')->get();
+
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
 
         return view('brandivjab.create', compact(['branches', 'divisions', 'jabatans', 'atasans']));
     }
 
     public function store(BrandivjabRequest $request): RedirectResponse
     {
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(2);
+
         if ($request->validated()) {
             $brandivjab = Brandivjab::create([
                 'branch_id' => $request->branch_id,
@@ -150,35 +182,49 @@ class BrandivjabController extends Controller implements HasMiddleware
                 'updated_by' => auth()->user()->email,
             ]);
 
+            if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
+
             if ($brandivjab) {
                 return redirect()->back()->with('success', __('messages.successadded') . ' 👉 ' . $brandivjab->jabatan->nama);
             }
         }
+
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
 
         return redirect()->back()->withInput()->with('error', 'Error occured while saving!');
     }
 
     public function show(Request $request): View
     {
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(2);
+
         $datas = Brandivjab::find(Crypt::decrypt($request->brandivjab));
         $atasan = Brandivjab::find($datas->atasan_id);
+
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
 
         return view('brandivjab.show', compact(['datas', 'atasan']));
     }
 
     public function edit(Request $request): View
     {
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(2);
+
         $datas = Brandivjab::find(Crypt::decrypt($request->brandivjab));
         $branches = Branch::where('isactive', 1)->orderBy('nama')->pluck('nama', 'id');
         $divisions = Division::where('isactive', 1)->orderBy('nama')->pluck('nama', 'id');
         $jabatans = Jabatan::where('isactive', 1)->orderBy('nama')->pluck('nama', 'id');
         $atasans = Brandivjab::where('isactive', 1)->orderBy('jabatan_id')->get();
 
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
+
         return view('brandivjab.edit', compact(['datas', 'branches', 'divisions', 'jabatans', 'atasans']));
     }
 
     public function update(BrandivjabRequest $request): RedirectResponse
     {
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(2);
+
         $brandivjab = Brandivjab::find(Crypt::decrypt($request->brandivjab));
 
         if ($request->validated()) {
@@ -193,31 +239,45 @@ class BrandivjabController extends Controller implements HasMiddleware
                 'updated_by' => auth()->user()->email,
             ]);
 
+            if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
+
             return redirect()->back()->with('success', __('messages.successupdated') . ' 👉 ' . $brandivjab->jabatan->nama);
         } else {
+            if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
+
             return redirect()->back()->withInput()->with('error', 'Error occured while updating!');
         }
     }
 
     public function delete(Request $request): View
     {
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(2);
+
         $datas = Brandivjab::find(Crypt::decrypt($request->brandivjab));
+
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
 
         return view('brandivjab.delete', compact(['datas']));
     }
 
     public function destroy(Request $request): RedirectResponse
     {
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(2);
+
         $brandivjab = Brandivjab::find(Crypt::decrypt($request->brandivjab));
 
         try {
             $brandivjab->delete();
         } catch (\Illuminate\Database\QueryException $e) {
+            if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
+
             if (str_contains($e->getMessage(), 'Integrity constraint violation')) {
                 return redirect()->route('brandivjab.index')->with('error', 'Integrity constraint violation');
             }
             return redirect()->route('brandivjab.index')->with('error', $e->getMessage());
         }
+
+        if (auth()->user()->profile->site == 'KP') $this->db_switch(1);
 
         return redirect()->route('brandivjab.index')
             ->with('success', __('messages.successdeleted') . ' 👉 ' . $brandivjab->nama);
