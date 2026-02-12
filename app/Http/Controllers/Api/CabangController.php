34eb2ea3f84a7e39336f3d1e8463a659ba782d6b
fazkha <mr.eko.handriyanto@gmail.com>
2026-02-12 09:13:59 +0700

    perbaikan API cabang, upload bukti transfer, part 34

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -152,12 +152,6 @@ class CabangController extends Controller
 
                     if ($hasFile) {
                         $image = $request->file('foto');
-                        $this->db_switch(1);
-
-                        return response()->json([
-                            'status' => 'success',
-                            'omzet' => $omzet,
-                        ]);
 
                         $imageName = $omzet->image_nama;
                         $deleteName = $omzet->image_nama;
