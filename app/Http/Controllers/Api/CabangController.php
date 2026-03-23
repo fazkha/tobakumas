5e5195638f4e7ce68246773b73be1fbd5a6b3131
fazkha <mr.eko.handriyanto@gmail.com>
2026-03-23 12:07:42 +0700

    perbaikan API office, penambahan end point "jenis izin pegawai", part 12

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -49,25 +49,7 @@ class CabangController extends Controller
     {
         $this->db_switch(2);
 
-        $validator = Validator::make($request->all(), [
-            'id' => ['required', 'integer', 'exists:users,id'],
-        ]);
-
-        if ($validator->fails()) {
-            $errors = $validator->errors();
-
-            $this->db_switch(1);
-
-            foreach ($errors->all() as $message) {
-                return response([
-                    'message' => $message
-                ], 422);
-            }
-        }
-
-        $data = $validator->validated();
-
-        $mitra = DB::select("CALL sp_mitra_by_pc(?)", [$data['id']]);
+        $mitra = DB::select("CALL sp_mitra_by_pc(?)", [$request->id]);
 
         $this->db_switch(1);
 
