fb15c680a375106c0376607f9e8e36ef71b4961d
fazkha <mr.eko.handriyanto@gmail.com>
2026-03-23 12:39:41 +0700

    perbaikan API office, penambahan end point "list mitra", part 2

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -50,6 +50,7 @@ class CabangController extends Controller
         $this->db_switch(2);
 
         $mitra = DB::select("CALL sp_mitra_by_pc(?)", [$request->id]);
+        dd($mitra);
 
         $this->db_switch(1);
 
