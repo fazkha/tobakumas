5c759f1343022af099aa8b6d0bb8589d3d43ec85
fazkha <mr.eko.handriyanto@gmail.com>
2026-03-23 12:38:07 +0700

    perbaikan API office, penambahan end point "list mitra", part 1

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -55,7 +55,7 @@ class CabangController extends Controller
 
         return [
             'status' => 'success',
-            'data' => $mitra
+            'data' => $mitra['result']
         ];
     }
 
