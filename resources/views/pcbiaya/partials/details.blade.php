5123cac68983b4f969db5ade16ce71413fe4dc76
fazkha <mr.eko.handriyanto@gmail.com>
2026-04-08 14:41:19 +0700

    penambahan menu baru finance (pcpettycash, pcbiaya) dan kepegawaian (mitraizin, pcizin), part 68

--- resources/views/pcbiaya/partials/details.blade.php
+++ resources/views/pcbiaya/partials/details.blade.php
@@ -14,17 +14,15 @@
                 <x-text-span>{{ $detail->jenis_nama }}</x-text-span>
             </td>
             <td class="align-top">
-                <span class="text-center">
-                    <label class="cursor-pointer flex flex-row gap-2 items-center">
-                        <input type="checkbox" name="approved_fin[]" value="{{ $detail->approved_fin }}"
-                            @php if ($i < count($details)) {
+                <label class="cursor-pointer flex flex-row gap-2 items-end">
+                    <input type="checkbox" name="approved_fin[]" value="{{ $detail->approved_fin }}"
+                        @php if ($i < count($details)) {
                         if ($details[$i]->approved_fin == $detail->approved_fin) {
                             echo 'checked';
                         }
                     } @endphp
-                            class="dark:border-white-400/20 dark:scale-100 transition-all duration-500 ease-in-out dark:hover:scale-110 dark:checked:scale-100 w-7 h-7 rounded-lg shadow-md">
-                    </label>
-                </span>
+                        class="dark:border-white-400/20 dark:scale-100 transition-all duration-500 ease-in-out dark:hover:scale-110 dark:checked:scale-100 w-7 h-7 rounded-lg shadow-md">
+                </label>
             </td>
         </tr>
         @php
