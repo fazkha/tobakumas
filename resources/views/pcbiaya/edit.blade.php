f81e8cddde9cd21da6f03b20413dac4184b28e33
fazkha <mr.eko.handriyanto@gmail.com>
2026-04-09 09:15:09 +0700

    penambahan menu baru finance (pcpettycash, pcbiaya) dan kepegawaian (mitraizin, pcizin), part 76

--- resources/views/pcbiaya/edit.blade.php
+++ resources/views/pcbiaya/edit.blade.php
@@ -29,77 +29,79 @@
         </h1>
     </div>
 
-    <div x-data="{
-        openModal: false,
-        imagePreview: '{{ asset('images/0cd6be830e32f80192d496e50cfa9dbc.jpg') }}',
-        modalTitle: 'Title'
-    }" class="w-full overflow-x-auto">
-        <div class="py-2 flex flex-col">
+    <form
+        action="{{ route('pcbiaya.updatee', ['branch_id' => Crypt::Encrypt($details[0]->branch_id), 'tanggal' => Crypt::Encrypt($details[0]->tanggal)]) }}"
+        method="POST" enctype="multipart/form-data">
+        @csrf
 
-            <div class="w-full px-4 py-2">
-                <div class="flex flex-col items-center">
+        <div x-data="{
+            openModal: false,
+            imagePreview: '{{ asset('images/0cd6be830e32f80192d496e50cfa9dbc.jpg') }}',
+            modalTitle: 'Title'
+        }" class="w-full overflow-x-auto">
+            <div class="py-2 flex flex-col">
 
-                    <div class="w-full" role="alert">
-                        @include('pcbiaya.partials.feedback')
-                    </div>
+                <div class="w-full px-4 py-2">
+                    <div class="flex flex-col items-center">
 
-                    {{-- Master --}}
-                    <div
-                        class="w-full shadow-lg bg-primary-50 rounded-md border border-primary-100 dark:bg-primary-900 dark:border-primary-800">
-                        <div class="p-4 space-y-2">
+                        <div class="w-full" role="alert">
+                            @include('pcbiaya.partials.feedback')
+                        </div>
 
-                            <div class="flex flex-col lg:flex-row">
-                                <div class="w-full lg:w-1/2 px-2">
+                        {{-- Master --}}
+                        <div
+                            class="w-full shadow-lg bg-primary-50 rounded-md border border-primary-100 dark:bg-primary-900 dark:border-primary-800">
+                            <div class="p-4 space-y-2">
 
-                                    <div class="w-auto pb-4">
-                                        <span for="branch_id"
-                                            class="block mb-2 font-medium text-primary-600 dark:text-primary-500">@lang('messages.branch')</span>
-                                        <x-text-span>{{ $details[0]->cabang->nama }}</x-text-span>
+                                <div class="flex flex-col lg:flex-row">
+                                    <div class="w-full lg:w-1/2 px-2">
+
+                                        <div class="w-auto pb-4">
+                                            <span for="branch_id"
+                                                class="block mb-2 font-medium text-primary-600 dark:text-primary-500">@lang('messages.branch')</span>
+                                            <x-text-span>{{ $details[0]->cabang->nama }}</x-text-span>
+                                        </div>
+
+                                        <div class="w-auto pb-4">
+                                            <span for="tanggal"
+                                                class="block mb-2 font-medium text-primary-600 dark:text-primary-500">@lang('messages.transactiondate')</span>
+                                            <x-text-span>{{ $details[0]->tanggal }}</x-text-span>
+                                        </div>
                                     </div>
 
-                                    <div class="w-auto pb-4">
-                                        <span for="tanggal"
-                                            class="block mb-2 font-medium text-primary-600 dark:text-primary-500">@lang('messages.transactiondate')</span>
-                                        <x-text-span>{{ $details[0]->tanggal }}</x-text-span>
-                                    </div>
-                                </div>
+                                    <div class="w-full lg:w-1/2 px-2 flex flex-col justify-start">
+                                        <div class="w-auto pb-4 lg:pb-12">
+                                            <span for="pc_nama"
+                                                class="block mb-2 font-medium text-primary-600 dark:text-primary-500">@lang('messages.pcname')</span>
+                                            <x-text-span>{{ $details[0]->pc_nama }}</x-text-span>
+                                        </div>
 
-                                <div class="w-full lg:w-1/2 px-2 flex flex-col justify-start">
-                                    <div class="w-auto pb-4 lg:pb-12">
-                                        <span for="pc_nama"
-                                            class="block mb-2 font-medium text-primary-600 dark:text-primary-500">@lang('messages.pcname')</span>
-                                        <x-text-span>{{ $details[0]->pc_nama }}</x-text-span>
-                                    </div>
-
-                                    <div class="flex flex-row flex-wrap items-center justify-end gap-2 md:gap-4">
-                                        <x-primary-button type="submit" class="block" tabindex="7">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="size-5">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
-                                            </svg>
-                                            <span class="pl-1">@lang('messages.save')</span>
-                                        </x-primary-button>
-                                        <x-anchor-secondary href="{{ route('pcbiaya.index') }}" tabindex="8">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="size-5">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M6 18 18 6M6 6l12 12" />
-                                            </svg>
-                                            <span class="pl-1">@lang('messages.close')</span>
-                                        </x-anchor-secondary>
+                                        <div class="flex flex-row flex-wrap items-center justify-end gap-2 md:gap-4">
+                                            <x-primary-button type="submit" class="block" tabindex="7">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                    class="size-5">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+                                                </svg>
+                                                <span class="pl-1">@lang('messages.save')</span>
+                                            </x-primary-button>
+                                            <x-anchor-secondary href="{{ route('pcbiaya.index') }}" tabindex="8">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                    class="size-5">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M6 18 18 6M6 6l12 12" />
+                                                </svg>
+                                                <span class="pl-1">@lang('messages.close')</span>
+                                            </x-anchor-secondary>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <form
-                action="{{ route('pcbiaya.updatee', ['branch_id' => Crypt::Encrypt($details[0]->branch_id), 'tanggal' => Crypt::Encrypt($details[0]->tanggal)]) }}"
-                method="POST" enctype="multipart/form-data">
-                @csrf
 
                 <div class="flex flex-col lg:flex-row gap-4 px-4 py-2">
                     <div class="w-full">
@@ -110,23 +112,22 @@
                                 class="w-full shadow-lg bg-primary-50 rounded-md border border-primary-100 dark:bg-primary-900 dark:border-primary-800">
                                 <div class="p-4 space-y-2">
                                     <div class="flex flex-row items-center gap-2">
-                                        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5"
-                                            viewBox="0 0 52 52" enable-background="new 0 0 52 52" xml:space="preserve">
-                                            <g>
+                                        <svg class="size-5" viewBox="0 0 32 32" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <g clip-path="url(#clip0_901_1341)">
                                                 <path
-                                                    d="M24.3,36.5c0.7,0,1.4,0.1,2,0.3L15.5,6.2c0,0,0,0,0,0l-1-3c-0.3-0.9-1.2-1.3-2-1L3.1,5.3 c-0.9,0.3-1.3,1.2-1,2l1,3c0.3,0.9,1.2,1.3,2,1L10,9.7l9.9,28.1C21.2,37,22.7,36.5,24.3,36.5z" />
-                                                <path
-                                                    d="M41.2,29.2l-9.9,3.5c-1,0.4-2.2-0.2-2.5-1.2l-3.5-9.9c-0.4-1,0.2-2.2,1.2-2.5l9.9-3.5 c1-0.4,2.2,0.2,2.5,1.2l3.5,9.9C42.8,27.7,42.2,28.8,41.2,29.2z" />
-                                                <path
-                                                    d="M31.8,12.9l-6.7,2.3c-1,0.4-2.2-0.2-2.5-1.2l-2.3-6.7c-0.4-1,0.2-2.2,1.2-2.5l6.7-2.3 c1-0.4,2.2,0.2,2.5,1.2l2.3,6.7C33.4,11.3,32.9,12.5,31.8,12.9z" />
-                                                <path
-                                                    d="M49.9,35.5l-1-3c-0.3-0.9-1.2-1.3-2-1l-18.2,6.3c1.9,1.2,3.2,3.2,3.6,5.5l16.7-5.7 C49.8,37.3,50.2,36.4,49.9,35.5z" />
-                                                <path
-                                                    d="M24.3,39.1c-3,0-5.5,2.5-5.5,5.5c0,3,2.5,5.5,5.5,5.5s5.5-2.5,5.5-5.5C29.8,41.5,27.3,39.1,24.3,39.1z" />
+                                                    d="M15 17C15 15.343 13.657 14 12 14M12 14C10.343 14 9 15.343 9 17C9 18.657 10.343 20 12 20C13.657 20 15 21.343 15 23C15 24.657 13.657 26 12 26M12 14V13M12 26C10.343 26 9 24.657 9 23M12 26V27M22 31H31V29M25 26H31V24M26 21H31V19M26 16H31V14M23 11H31V9M10 6H31V1H7V6M23 20C23 13.926 18.074 9 12 9C5.926 9 1 13.926 1 20C1 26.074 5.926 31 12 31C18.074 31 23 26.074 23 20Z"
+                                                    stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                    stroke-linejoin="round" />
                                             </g>
+                                            <defs>
+                                                <clipPath id="clip0_901_1341">
+                                                    <rect width="32" height="32" fill="white" />
+                                                </clipPath>
+                                            </defs>
                                         </svg>
                                         <span class="block font-medium text-primary-600 dark:text-primary-500">
-                                            @lang('messages.solditem')
+                                            @lang('messages.cost')
                                         </span>
                                     </div>
 
@@ -159,31 +160,31 @@
                         </div>
                     </div>
                 </div>
+            </div>
 
-            </form>
-        </div>
+    </form>
 
-        <div x-show.transition.duration.500ms="openModal"
-            class="fixed inset-0 flex items-center justify-center px-4 md:px-0 bg-white bg-opacity-75 dark:bg-black dark:bg-opacity-75">
-            <div @click.away="openModal = false"
-                class="flex flex-col p-6 h-auto w-auto shadow-2xl rounded-lg border-2 bg-white border-gray-400 dark:bg-gray-700 dark:border-gray-900">
-                <div class="flex justify-between mb-4">
-                    <div class="font-bold text-lg text-gray-900 dark:text-gray-50"><span x-html="modalTitle"></span>
-                    </div>
-                    <button @click="openModal = false">
-                        <svg class="w-5 h-5 text-gray-900 dark:text-gray-50" viewBox="0 0 24 24" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M5.293 5.293a1 1 0 0 1 1.414 0L12 10.586l5.293-5.293a1 1 0 1 1 1.414 1.414L13.414 12l5.293 5.293a1 1 0 0 1-1.414 1.414L12 13.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L10.586 12 5.293 6.707a1 1 0 0 1 0-1.414z"
-                                fill="currentColor" />
-                        </svg>
-                    </button>
+    <div x-show.transition.duration.500ms="openModal"
+        class="fixed inset-0 flex items-center justify-center px-4 md:px-0 bg-white bg-opacity-75 dark:bg-black dark:bg-opacity-75">
+        <div @click.away="openModal = false"
+            class="flex flex-col p-6 h-auto w-auto shadow-2xl rounded-lg border-2 bg-white border-gray-400 dark:bg-gray-700 dark:border-gray-900">
+            <div class="flex justify-between mb-4">
+                <div class="font-bold text-lg text-gray-900 dark:text-gray-50"><span x-html="modalTitle"></span>
                 </div>
-                <div class="flex items-center justify-center overflow-hidden rounded-lg">
-                    <img x-ref="imgRef" src="" class="w-auto h-full max-h-96" />
-                </div>
+                <button @click="openModal = false">
+                    <svg class="w-5 h-5 text-gray-900 dark:text-gray-50" viewBox="0 0 24 24" fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M5.293 5.293a1 1 0 0 1 1.414 0L12 10.586l5.293-5.293a1 1 0 1 1 1.414 1.414L13.414 12l5.293 5.293a1 1 0 0 1-1.414 1.414L12 13.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L10.586 12 5.293 6.707a1 1 0 0 1 0-1.414z"
+                            fill="currentColor" />
+                    </svg>
+                </button>
+            </div>
+            <div class="flex items-center justify-center overflow-hidden rounded-lg">
+                <img x-ref="imgRef" src="" class="w-auto h-full max-h-96" />
             </div>
         </div>
+    </div>
     </div>
 
     @push('scripts')
