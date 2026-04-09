ecf5b5983352dee6795af56b6833cf0654770d10
fazkha <mr.eko.handriyanto@gmail.com>
2026-04-09 10:34:46 +0700

    penambahan menu baru finance (pcpettycash, pcbiaya) dan kepegawaian (mitraizin, pcizin), part 88

--- resources/views/pcbiaya/edit.blade.php
+++ resources/views/pcbiaya/edit.blade.php
@@ -147,24 +147,25 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="flex flex-row flex-wrap items-center justify-end gap-2 md:gap-4">
-                            <x-primary-button type="submit" class="block" tabindex="7">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="size-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
-                                </svg>
-                                <span class="pl-1">@lang('messages.save')</span>
-                            </x-primary-button>
-                            <x-anchor-secondary href="{{ route('pcbiaya.index') }}" tabindex="8">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="size-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                                </svg>
-                                <span class="pl-1">@lang('messages.close')</span>
-                            </x-anchor-secondary>
+                            <div class="flex flex-row flex-wrap items-center justify-end gap-2 md:gap-4">
+                                <x-primary-button type="submit" class="block" tabindex="7">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+                                    </svg>
+                                    <span class="pl-1">@lang('messages.save')</span>
+                                </x-primary-button>
+                                <x-anchor-secondary href="{{ route('pcbiaya.index') }}" tabindex="8">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                    <span class="pl-1">@lang('messages.close')</span>
+                                </x-anchor-secondary>
+                            </div>
                         </div>
                     </div>
                 </div>
