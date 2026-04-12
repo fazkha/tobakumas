44e07da43646e9f4a05b1bbc1573f895602b029e
fazkha <mr.eko.handriyanto@gmail.com>
2026-04-12 16:20:06 +0700

    penambahan menu baru finance (pcpettycash, pcbiaya) dan kepegawaian (mitraizin, pcizin), part 178

--- resources/views/pcizin/partials/table.blade.php
+++ resources/views/pcizin/partials/table.blade.php
@@ -68,7 +68,8 @@
                         </td>
                         <td
                             class="px-3 py-1 text-sm border-b border-primary-100 bg-primary-20 dark:bg-primary-900 dark:border-primary-800">
-                            <span class="text-gray-900 dark:text-white">{{ $data->tanggal_mulai }}</span>
+                            <span
+                                class="text-gray-900 dark:text-white">{{ $data->tanggal_mulai->translatedFormat('l, d F Y') }}</span>
                         </td>
                         <td
                             class="px-3 py-1 text-sm border-b border-primary-100 bg-primary-20 dark:bg-primary-900 dark:border-primary-800">
