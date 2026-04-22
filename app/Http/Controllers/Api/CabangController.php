eecdb0fc896d18bb4e689ff1039b0f29ab7ee63f
fazkha <mr.eko.handriyanto@gmail.com>
2026-04-22 13:23:18 +0700

    perbaikan aplikasi mitra, pembacaan koordinat peta, part 3

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -1623,7 +1623,6 @@ class CabangController extends Controller
         $rute = null;
         $prev = null;
         $tgblth = $data['tahun'] . '-' . str_pad($data['bulan'], 2, "0", STR_PAD_LEFT) . '-' . $data['tanggal'];
-        dd($tgblth);
 
         try {
             $rute = RuteGerobak::join('users', 'rute_gerobaks.user_id', '=', 'users.id')
