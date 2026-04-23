a2b74c294fa8b6b093319dcce62e1961323e1eb6
fazkha <mr.eko.handriyanto@gmail.com>
2026-04-23 09:45:32 +0700

    perbaikan logo png, part 3

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -4,4 +4,4 @@
 </svg> --}}
 {{-- width="40" height="40" --}}
 
-<img src="/images/logo.png" {{ $attributes->merge(['class' => 'w-auto h-10 pb-[1px]']) }} />
+<img src="/images/logo.png" {{ $attributes->merge(['class' => 'w-auto h-[10px]']) }} />
