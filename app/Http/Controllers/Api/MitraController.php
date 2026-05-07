0bf6d21ff91fb7e72d2fa4802e37dd92fbb5fb6b
fazkha <mr.eko.handriyanto@gmail.com>
2026-05-07 14:49:30 +0700

    perbaikan aplikasi mobile mitra, penambahan pencapaian omzet dan bonus pekanan, part 9

--- app/Http/Controllers/Api/MitraController.php
+++ app/Http/Controllers/Api/MitraController.php
@@ -503,7 +503,7 @@ class MitraController extends Controller
         $pencapaian_sisa_hari = intval($today->diffInDays($endDate, false)) - 1;
         $pencapaian_omzet_phari = (($mitraAverageOmzet ? $mitraAverageOmzet->target_akum_omzet : 0) - $akum_omzet) / ($pencapaian_sisa_hari > 0 ? $pencapaian_sisa_hari : 1);
 
-        dd($pct_akum_omzet, $pencapaian_sisa_hari, $pencapaian_omzet_phari);
+        // dd($pct_akum_omzet, $pencapaian_sisa_hari, $pencapaian_omzet_phari);
 
         if ($found) {
             $found->update([
