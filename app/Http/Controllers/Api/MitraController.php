29ad99bc25f9fc1d16e45deeb86d33b4f55989c3
fazkha <mr.eko.handriyanto@gmail.com>
2026-05-14 14:30:44 +0700

    perbaikan aplikasi mobile mitra, penambahan pencapaian omzet dan bonus pekanan, part 96

--- app/Http/Controllers/Api/MitraController.php
+++ app/Http/Controllers/Api/MitraController.php
@@ -936,10 +936,8 @@ class MitraController extends Controller
         }
 
         $data = $validator->validated();
-        $app_setting = AppSetting::where('parm', 'mitra_pembagi_rata2_omzet')->first();
-        $pembagi = $app_setting ? $app_setting->value : 6;
 
-        $omzet = DB::select("CALL sp_mitra_omset_pekanan(?,?)", [$data['id'], $pembagi]);
+        $omzet = DB::select("CALL sp_mitra_omset_pekanan(?,?)", [$data['id']]);
         $trend = null;
         $pct = null;
         $trend_bonus = null;
