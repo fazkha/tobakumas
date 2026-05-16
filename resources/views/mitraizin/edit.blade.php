678d41d54cf873210bd928d50089cac7a737dde7
fazkha <mr.eko.handriyanto@gmail.com>
2026-05-16 08:54:35 +0700

    perbaikan aplikasi api, upload gambar form izin, part 5

--- resources/views/mitraizin/edit.blade.php
+++ resources/views/mitraizin/edit.blade.php
@@ -52,16 +52,16 @@
                                             class="block mb-2 font-medium text-primary-600 dark:text-primary-500">@lang('messages.mitraname')</span>
                                         <x-text-span>{{ $datas->mitra_nama }}</x-text-span>
                                     </div>
-                                </div>
 
-                                <div class="w-full lg:w-1/2 px-2 flex flex-col justify-start">
                                     <div class="w-auto pb-4">
                                         <span for="jenis_id"
                                             class="block mb-2 font-medium text-primary-600 dark:text-primary-500">@lang('messages.izin')</span>
                                         <x-text-span>{{ $datas->jenis_nama }}</x-text-span>
                                     </div>
+                                </div>
 
-                                    <div class="w-auto pb-4 lg:pb-12">
+                                <div class="w-full lg:w-1/2 px-2 flex flex-col justify-start">
+                                    <div class="w-auto pb-4">
                                         <div class="flex flex-row flex-wrap items-center gap-2">
                                             <div>
                                                 <span for="tanggal_mulai"
@@ -78,6 +78,16 @@
                                                     class="text-bold">{{ $datas->tanggal_selesai->translatedFormat('H:i') }}</x-text-span>
                                             </div>
                                         </div>
+                                    </div>
+
+                                    <div class="w-auto pb-4 lg:pb-12">
+                                        <x-text-span>
+                                            <div class="flex justify-center">
+                                                <img id="image-preview" class="w-full lg:w-3/5 h-auto border rounded-lg"
+                                                    @if ($datas->image_nama) src="{{ asset($datas->image_lokasi . '/' . $datas->image_nama) }}" @else src="{{ url('/') }}/images/0cd6be830e32f80192d496e50cfa9dbc.jpg" @endif
+                                                    alt="o.o" />
+                                            </div>
+                                        </x-text-span>
                                     </div>
 
                                     <div class="flex flex-row flex-wrap items-center justify-end gap-2 md:gap-4">
