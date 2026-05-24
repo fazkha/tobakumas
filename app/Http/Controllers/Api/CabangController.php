1f73656ae27ac029cadbc3481d49986add386408
fazkha <mr.eko.handriyanto@gmail.com>
2026-05-24 10:21:19 +0700

    perbaikan aplikasi web gudang, penambahan menu tambah/ganti hari, part 29

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -1467,7 +1467,6 @@ class CabangController extends Controller
 
         $total_omzet = $result->total_omzet;
         $jumlah_hari = $result->jumlah_hari;
-        dd($startDate, $endDate, $total_omzet, $jumlah_hari);
 
         $mitraAverageOmzet = MitraAverageOmzet::where('user_id', $mitra_user_id)
             ->where('minggu', $yearWeek)
@@ -1517,6 +1516,7 @@ class CabangController extends Controller
 
             $total_omzet = $result->total_omzet;
             $jumlah_hari = $result->jumlah_hari;
+            dd($startDate, $endDate, $total_omzet, $jumlah_hari);
 
             $mitraAverageOmzet = MitraAverageOmzet::where('user_id', $mitra_user_id)
                 ->where('minggu', $yearWeek)
