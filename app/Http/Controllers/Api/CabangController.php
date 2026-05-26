b5f9cd5c59138c0d5c2187ce23be206d66b3ee84
fazkha <mr.eko.handriyanto@gmail.com>
2026-05-26 14:35:29 +0700

    perbaikan aplikasi mobile PC, penambahan KPI PC, part 39

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -1676,7 +1676,7 @@ class CabangController extends Controller
             'romzet' => $rata2,
             'bonus' => $bonus,
             'jumlah_hari' => $jh,
-            'pct' => ($jh / $val_hari_pbulan) * 100,
+            'pct' => round(($jh / $val_hari_pbulan) * 100, 2),
         ]);
     }
 
