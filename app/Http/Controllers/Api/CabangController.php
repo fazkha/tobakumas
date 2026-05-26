04e1af87eae6644745ab3400e916050d3f16df50
fazkha <mr.eko.handriyanto@gmail.com>
2026-05-26 10:29:32 +0700

    perbaikan aplikasi mobile PC, penambahan KPI PC, part 24

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -1635,7 +1635,7 @@ class CabangController extends Controller
                     ->selectRaw('SUM(total_hpp) AS hpp')
                     ->first();
 
-                $modal = $data ? $data->hpp : 0;
+                $modal = $data ? $data->hpp / 1000 : 0;
 
                 dd($rata2, $gapok, $modal);
                 DB::select("CALL sp_pc_target_bonus(?,?)", [$rata2, $gapok, $modal]);
