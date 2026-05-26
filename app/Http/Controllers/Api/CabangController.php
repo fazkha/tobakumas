f1c9ec6c9fe6209c9c344a6b9710466053687b80
fazkha <mr.eko.handriyanto@gmail.com>
2026-05-26 08:30:05 +0700

    perbaikan aplikasi mobile PC, penambahan KPI PC, part 19

--- app/Http/Controllers/Api/CabangController.php
+++ app/Http/Controllers/Api/CabangController.php
@@ -1549,17 +1549,23 @@ class CabangController extends Controller
 
             // if ($found->approved_omzet == 1) {
             $rata2 = $total_omzet / $jumlah_hari;
+            // $gaji_pokok = DB::table('users as u')
+            //     ->join('pegawais as p1', 'p1.email', '=', 'u.email')
+            //     ->join('pegawai_gajis as p2', 'p2.pegawai_id', '=', 'p1.id')
+            //     ->where('u.id', $data['pc_id'])
+            //     ->select('p2.gaji_pokok')
+            //     ->get();
             $gaji_pokok = DB::table('users as u')
                 ->join('pegawais as p1', 'p1.email', '=', 'u.email')
-                ->join('pegawai_gajis as p2', 'p2.pegawai_id', '=', 'p1.id')
-                ->where('u.id', 40)
-                ->select('p2.gaji_pokok')
-                ->first();
+                ->where('u.id', $data['pc_id'])
+                ->select('p1.id')
+                ->get();
             // ->value('p2.gaji_pokok');
             // ->join('pegawais', 'pegawais.email', '=', 'users.email')
             // ->selectRaw('pg.gaji_pokok')
             // ->join('pegawai_gajis as pg', 'pg.pegawai_id', '=', 'pegawais.id')
             dd($rata2, $gaji_pokok);
+
             $gapok = $pegawai ? $pegawai->gaji_pokok : 0;
             dd($rata2, $gapok, $pegawai);
             $hpp = 0;
