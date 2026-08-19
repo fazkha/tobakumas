df9929351addc56c8d62c46232d0b293ee0b6916
fazkha <mr.eko.handriyanto@gmail.com>
2026-08-19 10:37:31 +0700

    next commit

---
+++ app/Services/GoogleSheetService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services;
+
+use Google\Client;
+use Google\Service\Sheets;
+
+class GoogleSheetService
+{
+    protected Sheets $service;
+
+    public function __construct()
+    {
+        $client = new Client();
+
+        $client->setApplicationName(
+            config('app.name')
+        );
+
+        $credentials = storage_path(
+            'app/' . config('google.sheets.credentials')
+        );
+
+        $client->setAuthConfig($credentials);
+
+        $client->setScopes([
+            Sheets::SPREADSHEETS_READONLY,
+        ]);
+
+        $this->service = new Sheets($client);
+    }
+
+    public function getValues(
+        string $range
+    ): array {
+
+        $spreadsheetId = config(
+            'google.sheets.spreadsheet_id'
+        );
+
+        $response = $this->service
+            ->spreadsheets
+            ->values
+            ->get(
+                $spreadsheetId,
+                $range
+            );
+
+        return $response->getValues() ?? [];
+    }
+}
