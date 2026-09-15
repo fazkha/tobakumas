f95c035c858be0e40af8fb375e2166e086b83455
fazkha <mr.eko.handriyanto@gmail.com>
2026-09-15 14:38:58 +0700

    next commit

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,10 @@
 
 // use Illuminate\Foundation\Inspiring;
 // use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
+// use Illuminate\Support\Facades\Schedule;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('google-sheet:sync-data-from-google-sheet')->dailyAt('22:00');
+// Schedule::command('google-sheet:sync-data-from-google-sheet')->dailyAt('22:00');
